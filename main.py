36b5c3262c95d4b0a2870858efc07135f8962164
kelshardy <k.hardy44@outlook.com>
2023-04-24 22:44:29 +1000

    intial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("""
+888              888    d8b              888b     d888          888                    888888b.                     888      
+888              888    88P              8888b   d8888          888                    888  "88b                    888      
+888              888    8P               88888b.d88888          888                    888  .88P                    888      
+888      .d88b.  888888 "  .d8888b       888Y88888P888  8888b.  888  888  .d88b.       8888888K.   8888b.  88888b.  888  888 
+888     d8P  Y8b 888       88K           888 Y888P 888     "88b 888 .88P d8P  Y8b      888  "Y88b     "88b 888 "88b 888 .88P 
+888     88888888 888       "Y8888b.      888  Y8P  888 .d888888 888888K  88888888      888    888 .d888888 888  888 888888K  
+888     Y8b.     Y88b.          X88      888   "   888 888  888 888 "88b Y8b.          888   d88P 888  888 888  888 888 "88b 
+88888888 "Y8888   "Y888     88888P'      888       888 "Y888888 888  888  "Y8888       8888888P"  "Y888888 888  888 888  888 
+                                                                                                                             
+                                                                                                                             
+                                                                                                                             
+""")
+input("Press Enter to Start...")
+
+your_name = input("Hello! Welcome to Let's Make Bank! I'm you're host, Frank, and you are? Enter name: ")
+print(f"Welcome {your_name}! Let's get this show on the road! ")
