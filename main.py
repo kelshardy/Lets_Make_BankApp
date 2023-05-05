b289fe40aa87f2ce0ce42dcb82a15d49e50b8082
kelshardy <k.hardy44@outlook.com>
2023-05-05 16:11:03 +1000

    color and styling updated

--- main.py
+++ main.py
@@ -256,7 +256,16 @@ def category_menu():
 
     while True:
         if correct_categories >= 3:
-            print("You are the bread winner!")
+            print(f"""{fg('71')}
+888       888 d8b                                    888 
+888   o   888 Y8P                                    888 
+888  d8b  888                                        888 
+{fg('2')}888 d888b 888 888 88888b.  88888b.   .d88b.  888d888 888 
+888d88888b888 888 888 "88b 888 "88b d8P  Y8b 888P"   888 
+88888P Y88888 888 888  888 888  888 88888888 888     Y8P 
+{fg('29')}8888P   Y8888 888 888  888 888  888 Y8b.     888      "  
+888P     Y888 888 888  888 888  888  "Y8888  888     888
+{attr('reset')}""")
             return
 
         user_decision = input("What category would you like to play?: ")
